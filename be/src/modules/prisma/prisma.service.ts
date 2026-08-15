044e6d2469251e7adb0759c574f4d74ae3f5741f
Khalifa Al Hasan <khlfaalhsn5@gmail.com>
2026-08-16 03:37:39 +0700

    fix(be/prisma): fix import order in PrismaService
    - Move PrismaClient import after PrismaPg adapter

--- be/src/modules/prisma/prisma.service.ts
+++ be/src/modules/prisma/prisma.service.ts
@@ -4,8 +4,8 @@ import {
   OnModuleDestroy,
   OnModuleInit,
 } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService
