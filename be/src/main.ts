d0a46aece844fc47f4c9d60ae6114b3a6c8fd9e7
Khalifa Al Hasan <khlfaalhsn5@gmail.com>
2026-08-16 03:37:54 +0700

    feat(be): add PrismaExceptionFilter and disable bodyParser in main bootstrap
    - Register PrismaExceptionFilter as global filter
    - Set bodyParser: false for better-auth raw body handling

--- be/src/main.ts
+++ be/src/main.ts
@@ -3,12 +3,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 import { Logger, LoggerErrorInterceptor } from 'nestjs-pino';
+import { PrismaExceptionFilter } from './common/filter/prisma-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     bufferLogs: true,
+    bodyParser: false,
   });
 
+  app.useGlobalFilters(new PrismaExceptionFilter());
   app.useLogger(app.get(Logger));
   app.useGlobalInterceptors(new LoggerErrorInterceptor());
 
