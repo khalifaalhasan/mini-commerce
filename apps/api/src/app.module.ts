3cdd37e278e95cfa1af3c437f2392c7a796610a1
Khalifa Al Hasan <khlfaalhsn5@gmail.com>
2026-08-16 16:41:19 +0700

    wip(api): scaffold category module and wire to app module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,52 +1,20 @@
+// src/app.module.ts
 import { Module } from "@nestjs/common";
+import { LoggerModule } from "nestjs-pino";
 import { ProductModule } from "./modules/product/product.module";
 import { PrismaModule } from "./modules/prisma/prisma.module";
-import { LoggerModule } from "nestjs-pino";
 import { AuthModule } from "@thallesp/nestjs-better-auth";
+import { CategoryModule } from "./modules/category/category.module";
 import { auth } from "./modules/auth/auth";
-
-const isDev = process.env.NODE_ENV !== "production";
+import { logConfig } from "@mini-commerce/logger";
 
 @Module({
   imports: [
-    LoggerModule.forRoot({
-      pinoHttp: {
-        level: isDev ? "debug" : "info",
-        transport: isDev
-          ? {
-              target: "pino-pretty",
-              options: {
-                colorize: true,
-                singleLine: false,
-                translateTime: "SYS:HH:MM:ss",
-                ignore: "pid,hostname",
-                messageKey: "msg",
-                levelFirst: true,
-              },
-            }
-          : undefined,
-        serializers: {
-          req(req) {
-            return {
-              method: req.method,
-              url: req.url,
-            };
-          },
-          res(res) {
-            return { statusCode: res.statusCode };
-          },
-        },
-        customProps: () => ({
-          context: "HTTP",
-        }),
-        autoLogging: {
-          ignore: (req) => req.url === "/api/health",
-        },
-      },
-    }),
+    LoggerModule.forRoot(logConfig),
+    AuthModule.forRoot({ auth }),
     ProductModule,
     PrismaModule,
-    AuthModule.forRoot({ auth }),
+    CategoryModule,
   ],
 })
 export class AppModule {}
